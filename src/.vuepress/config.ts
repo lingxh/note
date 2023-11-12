b51dae650c890f9a4717c2e90e0b06991dca4edf
lingxh <admin@lingxh.com>
2023-11-12 18:38:03 +0800

    update font

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -33,6 +33,18 @@ export default({
     });
       `,
     ],
+        ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
   ]
   
   // Enable it with pwa
