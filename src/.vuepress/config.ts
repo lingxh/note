a8f32da7e9d099bbd0462c810407b9ddd4cc7ead
lingxh <admin@lingxh.com>
2023-11-12 21:34:24 +0800

    config font

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -41,7 +41,7 @@ export default({
     [
       "link",
       {
-        href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap",
         rel: "stylesheet",
       },
     ],
